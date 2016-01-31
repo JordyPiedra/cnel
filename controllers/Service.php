5487709adc118a575cf887ed62cad7a3291bdf08
Jaime Vilca <jaime_v@outlook.com>
2016-01-31 09:39:34 -0500

    Desarrollo de controlador servicio

---
+++ controllers/Service.php
@@ -0,0 +1,41 @@
+<?php
+
+Class Service extends Controller{
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function information($data){
+		$data = explode('-', $data);
+	
+		switch ($data[0]) {
+			case 'canton':
+				$cantones = $this->model->getAllCantonById($data[1]);
+				echo json_encode($cantones);
+			break;
+
+			case 'parroquia':
+				$cantones = $this->model->getAllParroquiasById($data[1]);
+				echo json_encode($cantones);
+			break;
+			
+			default:
+			break;
+		}
+		
+		
+	}
+
+
+	public function test($id){
+		
+
+		$test = $this->model->delete($id);
+		var_dump($test);
+
+		
+		
+	}
+}
+
+?>
