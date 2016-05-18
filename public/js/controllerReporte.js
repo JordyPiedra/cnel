1c11f73816020bff77a26345356a030a57d4231f
Jordy <jordypiedra@gmail.com>
2016-05-18 17:56:22 -0500

    js

---
+++ public/js/controllerReporte.js
@@ -0,0 +1,5 @@
+function genera_chart(){
+    console.log($('#parametros').serialize());
+    $('#modal1').openModal();
+    //fajax({'IDCON_': IDCONC , 'CONTOKEN':CONTOKEN,'IDBCON':IDBCON,'IDBCONTOKEN':IDBCONTOKEN}, URL+"/Management/finaliza_fase", finaliza_fase_response);
+  }
